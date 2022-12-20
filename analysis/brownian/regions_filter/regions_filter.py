82e4e65aaec9c2826eb0ca6d2e2319b9ddbb8212
Marc Singleton <marcsingleton@berkeley.edu>
2022-12-20 13:01:53 -0600

    Updated regions_filter.py to use missing trims

--- analysis/brownian/regions_filter/regions_filter.py
+++ analysis/brownian/regions_filter/regions_filter.py
@@ -6,20 +6,6 @@ import os
 from src.utils import read_fasta
 
 
-def load_posteriors(path):
-    posteriors = {}
-    with open(path) as file:
-        field_names = file.readline().rstrip('\n').split('\t')
-        for line in file:
-            fields = {key: value for key, value in zip(field_names, line.rstrip('\n').split('\t'))}
-            ppid, p0, p1 = fields['ppid'], float(fields['0']), float(fields['1'])
-            try:
-                posteriors[ppid].append((p0, p1))
-            except KeyError:
-                posteriors[ppid] = [(p0, p1)]
-    return posteriors
-
-
 def spid_filter(spids):
     conditions = [({'dnov', 'dvir'}, 1),
                   ({'dmoj', 'dnav'}, 1),
@@ -36,12 +22,24 @@ def spid_filter(spids):
     return all([len(spids & group) >= num for group, num in conditions])
 
 
-ppid_regex = r'ppid=([A-Za-z0-9_]+)'
+def has_overlap(start1, stop1, start2, stop2):
+    if stop1 < start1:
+        raise ValueError('stop1 < start1')
+    if stop2 < start2:
+        raise ValueError('stop2 < start2')
+    if start1 == stop1 or start2 == stop2:
+        return False
+    if stop2 < stop1:
+        start1, start2 = start2, start1
+        stop1, stop2 = stop2, stop1
+    return stop1 > start2
+
+
+ppid_regex = r'ppid=([A-Za-z0-9_.]+)'
 spid_regex = r'spid=([a-z]+)'
 
-cutoff = 0.99
 spid_min = 20
-alphabet = {'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'}
+alphabet = {'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', '-', '.'}
 
 # Load regions
 OGid2regions = {}
@@ -56,36 +54,49 @@ with open('../get_regions/out/regions.tsv') as file:
             OGid2regions[OGid] = [(start, stop, disorder)]
 
 # Filter regions
-record_sets = {i: [] for i in range(10, 35, 5)}
+record_sets = {min_length: [] for min_length in range(10, 105, 5)}
 for OGid, regions in OGid2regions.items():
-    msa = read_fasta(f'../../../data/alignments/fastas/{OGid}.afa')
-    posteriors = load_posteriors(f'../../ortho_MSA/deletion_decode/out/{OGid}.tsv')
+    # Load MSA
+    msa = []
+    for header, seq in read_fasta(f'../../../data/alignments/fastas/{OGid}.afa'):
+        ppid = re.search(ppid_regex, header).group(1)
+        spid = re.search(spid_regex, header).group(1)
+        msa.append({'ppid': ppid, 'spid': spid, 'seq': seq})
+
+    # Get missing segments
+    ppid2trims = {}
+    with open(f'../../../data/alignments/trims/{OGid}.tsv') as file:
+        field_names = file.readline().rstrip('\n').split('\t')
+        for line in file:
+            fields = {key: value for key, value in zip(field_names, line.rstrip('\n').split('\t'))}
+            trims = []
+            for trim in fields['slices'].split(','):
+                if trim:
+                    start, stop = trim.split('-')
+                    trims.append((int(start), int(stop)))
+            ppid2trims[fields['ppid']] = trims
 
     for region in regions:
         # Get indices and length
-        start, stop = region[0], region[1]
-        length = stop - start
+        region_start, region_stop = region[0], region[1]
         disorder = region[2]
 
         # Extract and filter segments
-        segment_sets = {i: [] for i in record_sets}
-        for header, seq in msa:
-            # Extract
-            ppid = re.search(ppid_regex, header).group(1)
-            spid = re.search(spid_regex, header).group(1)
-            segment = seq[start:stop].upper()
+        segment_sets = {min_length: [] for min_length in record_sets}
+        for record in msa:
+            ppid, spid, segment = record['ppid'], record['spid'], record['seq'][region_start:region_stop]
+            trims = ppid2trims[ppid]
 
-            # Filter
+            # Filter by length, symbols, and overlap with missing trims
             length, is_standard = 0, True
             for sym in segment:
                 if sym not in ['-', '.']:
                     length += 1
                 if sym not in alphabet:
                     is_standard = False
-                    break
-            posterior = posteriors[ppid][start:stop]
+            no_missing = all([not has_overlap(region_start, region_stop, trim_start, trim_stop) for trim_start, trim_stop in trims])
             for min_length, segments in segment_sets.items():
-                if length >= min_length and is_standard and all([p1 < cutoff for _, p1 in posterior]):
+                if length >= min_length and is_standard and no_missing:
                     segments.append((ppid, spid))
 
         # Filter by phylogenetic diversity
@@ -93,7 +104,7 @@ for OGid, regions in OGid2regions.items():
             ppids = [ppid for ppid, _ in segments]
             spids = {spid for _, spid in segments}
             if len(spids) >= spid_min and spid_filter(spids):
-                record_sets[min_length].append((OGid, str(start), str(stop), disorder, ','.join(ppids)))
+                record_sets[min_length].append((OGid, str(region_start), str(region_stop), disorder, ','.join(ppids)))
 
 # Write records to file
 if not os.path.exists('out/'):
