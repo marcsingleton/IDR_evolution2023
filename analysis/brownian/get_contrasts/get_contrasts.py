a2ef26e51785ee767f62f73c1de66b7b93c70c67
Marc Singleton <marcsingleton@berkeley.edu>
2022-12-21 17:19:17 -0600

    Updated get_contrasts.py to calculate contrasts for all regions

--- analysis/brownian/get_contrasts/get_contrasts.py
+++ analysis/brownian/get_contrasts/get_contrasts.py
@@ -2,49 +2,31 @@
 
 import multiprocessing as mp
 import os
+import re
 
 import pandas as pd
 import skbio
+from src.utils import get_contrasts, read_fasta
 
 
-def get_args(grouped, tree):
+def get_args(grouped, tree, feature_labels):
     for name, group in grouped:
-        yield name, group, tree.copy()
-
-
-def get_contrasts(node):
-    child1, child2 = node.children
-    if child1.is_tip():
-        contrasts1, val1, bl1 = [], child1.value, child1.length
-    else:
-        contrasts1, val1, bl1 = get_contrasts(child1)
-    if child2.is_tip():
-        contrasts2, val2, bl2 = [], child2.value, child2.length
-    else:
-        contrasts2, val2, bl2 = get_contrasts(child2)
-
-    bl_sum = bl1 + bl2
-    value = (val1 * bl2 + val2 * bl1) / bl_sum
-    branch_length = node.length + bl1 * bl2 / bl_sum
-    contrasts = contrasts1 + contrasts2
-    contrasts.append((val1 - val2) / bl_sum)
-
-    return contrasts, value, branch_length
+        yield name, group, tree.copy(), feature_labels
 
 
 def apply_contrasts(args):
-    name, group, tree = args
+    name, group, tree, feature_labels = args
 
     # Map features to tips
     tree = tree.shear(group['spid'])
-    spid2idx = {spid: i for i, spid in enumerate(group['spid'])}
+    spid2idx = {spid: idx for idx, spid in zip(group.index, group['spid'])}
     for tip in tree.tips():
         idx = spid2idx[tip.name]
-        tip.value = group.iloc[idx].drop(['OGid', 'start', 'stop', 'ppid', 'spid'])
+        tip.value = group.loc[idx, feature_labels]
 
     # Get contrasts
     tree.length = 0  # Set root length to 0 for convenience
-    contrasts, value, _ = get_contrasts(tree)
+    contrasts, value = get_contrasts(tree)
 
     return name, contrasts, value
 
@@ -52,14 +34,27 @@ def apply_contrasts(args):
 num_processes = int(os.environ.get('SLURM_CPUS_ON_NODE', 1))
 tree_template = skbio.read('../../../data/trees/consensus_LG/100R_NI.nwk', 'newick', skbio.TreeNode)
 
+ppid_regex = r'ppid=([A-Za-z0-9_.]+)'
+spid_regex = r'spid=([a-z]+)'
+length_regex = r'regions_([0-9]+).tsv'
+
 if __name__ == '__main__':
     # Load sequence data
     ppid2spid = {}
-    with open('../../ortho_search/sequence_data/out/sequence_data.tsv') as file:
-        field_names = file.readline().rstrip('\n').split('\t')
-        for line in file:
-            fields = {key: value for key, value in zip(field_names, line.rstrip('\n').split('\t'))}
-            ppid2spid[fields['ppid']] = fields['spid']
+    OGids = sorted([path.removesuffix('.afa') for path in os.listdir('../../../data/alignments/fastas/') if path.endswith('.afa')])
+    for OGid in OGids:
+        for header, _ in read_fasta(f'../../../data/alignments/fastas/{OGid}.afa'):
+            ppid = re.search(ppid_regex, header).group(1)
+            spid = re.search(spid_regex, header).group(1)
+            ppid2spid[ppid] = spid
+
+    # Get minimum lengths
+    min_lengths = []
+    for path in os.listdir('../regions_filter/out/'):
+        match = re.search(length_regex, path)
+        if match:
+            min_lengths.append(int(match.group(1)))
+    min_lengths = sorted(min_lengths)
 
     # Load features
     features = pd.read_table('../get_features/out/features.tsv')
@@ -67,42 +62,47 @@ if __name__ == '__main__':
     features.loc[features['omega'] == -1, 'omega'] = 1
     feature_labels = list(features.columns.drop(['OGid', 'start', 'stop', 'ppid']))
 
-    # Load segments
+    # Load regions
     rows = []
-    with open('../aucpred_filter/out/regions_30.tsv') as file:
-        field_names = file.readline().rstrip('\n').split('\t')
-        for line in file:
-            fields = {key: value for key, value in zip(field_names, line.rstrip('\n').split('\t'))}
-            for ppid in fields['ppids'].split(','):
-                rows.append({'OGid': fields['OGid'], 'start': int(fields['start']), 'stop': int(fields['stop']),
-                             'ppid': ppid, 'spid': ppid2spid[ppid]})
-    segments = pd.DataFrame(rows).merge(features, how='left', on=['OGid', 'start', 'stop', 'ppid'])
-    regions = segments.groupby(['OGid', 'start', 'stop'])
+    for min_length in min_lengths:
+        with open(f'../regions_filter/out/regions_{min_length}.tsv') as file:
+            field_names = file.readline().rstrip('\n').split('\t')
+            for line in file:
+                fields = {key: value for key, value in zip(field_names, line.rstrip('\n').split('\t'))}
+                OGid, start, stop, disorder = fields['OGid'], int(fields['start']), int(fields['stop']), fields['disorder'] == 'True'
+                for ppid in fields['ppids'].split(','):
+                    rows.append({'OGid': OGid, 'start': start, 'stop': stop, 'disorder': disorder,
+                                 'ppid': ppid, 'spid': ppid2spid[ppid], 'min_length': min_length})
+    df = pd.DataFrame(rows)
 
-    # Apply contrasts
-    args = get_args(regions, tree_template)
-    with mp.Pool(processes=num_processes) as pool:
-        # Using imap distributes the construction of the args tuples
-        # However to force computation before pool is closed we must call list on it
-        records = list(pool.imap(apply_contrasts, args, chunksize=50))
-
-    # Write contrasts and means to file
     if not os.path.exists('out/'):
         os.mkdir('out/')
 
-    with open('out/contrasts.tsv', 'w') as file1, open('out/roots.tsv', 'w') as file2:
-        file1.write('\t'.join(['OGid', 'start', 'stop', 'contrast_id'] + feature_labels) + '\n')
-        file2.write('\t'.join(['OGid', 'start', 'stop'] + feature_labels) + '\n')
-        for name, contrasts, value in records:
-            # Write contrasts
-            for idx, contrast in enumerate(contrasts):
-                fields1 = [name[0], str(name[1]), str(name[2]), str(idx)]
-                for feature_label in feature_labels:
-                    fields1.append(str(contrast[feature_label]))
-                file1.write('\t'.join(fields1) + '\n')
+    for min_length in min_lengths:
+        segments = df[df['min_length'] == min_length].merge(features, how='left', on=['OGid', 'start', 'stop', 'ppid']).drop('min_length', axis=1)
+        regions = segments.groupby(['OGid', 'start', 'stop', 'disorder'])
 
-            # Write means
-            fields2 = [name[0], str(name[1]), str(name[2])]
-            for feature_label in feature_labels:
-                fields2.append(str(value[feature_label]))
-            file2.write('\t'.join(fields2) + '\n')
+        # Apply contrasts
+        args = get_args(regions, tree_template, feature_labels)
+        with mp.Pool(processes=num_processes) as pool:
+            # Using imap distributes the construction of the args tuples
+            # However to force computation before pool is closed we must call list on it
+            records = list(pool.imap(apply_contrasts, args, chunksize=50))
+
+        # Write contrasts and means to file
+        with open(f'out/contrasts_{min_length}.tsv', 'w') as file1, open(f'out/roots_{min_length}.tsv', 'w') as file2:
+            file1.write('\t'.join(['OGid', 'start', 'stop', 'contrast_id'] + feature_labels) + '\n')
+            file2.write('\t'.join(['OGid', 'start', 'stop'] + feature_labels) + '\n')
+            for name, contrasts, value in records:
+                # Write contrasts
+                for idx, contrast in enumerate(contrasts):
+                    fields1 = [name[0], str(name[1]), str(name[2]), str(idx)]
+                    for feature_label in feature_labels:
+                        fields1.append(str(contrast[feature_label]))
+                    file1.write('\t'.join(fields1) + '\n')
+
+                # Write means
+                fields2 = [name[0], str(name[1]), str(name[2])]
+                for feature_label in feature_labels:
+                    fields2.append(str(value[feature_label]))
+                file2.write('\t'.join(fields2) + '\n')
