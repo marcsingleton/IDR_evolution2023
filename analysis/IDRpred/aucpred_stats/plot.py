ef13ae86e0f342d5f861c2c40580380e191b098f
Marc Singleton <marcsingleton@berkeley.edu>
2023-03-23 17:23:14 -0700

    Moved aucpred_stats/ back to IDRpred/

--- analysis/IDRpred/aucpred_stats/plot.py
+++ analysis/IDRpred/aucpred_stats/plot.py
@@ -1,4 +1,4 @@
-"""Plot statistics of AUCpreD scores."""
+"""Plot statistics of AUCpreD Brownian motion parameters."""
 
 import os
 import re
@@ -124,7 +124,7 @@ for row in examples.itertuples():
     aligned_scores = np.full((len(msa), len(msa[0]['seq'])), np.nan)
     for i, record in enumerate(msa):
         ppid, seq = record['ppid'], record['seq']
-        scores = load_scores(f'../../IDRpred/aucpred_scores/out/{row.OGid}/{ppid}.diso_noprof')  # Remove anything after trailing .
+        scores = load_scores(f'../aucpred_scores/out/{row.OGid}/{ppid}.diso_noprof')  # Remove anything after trailing .
         idx = 0
         for j, sym in enumerate(seq):
             if sym not in ['-', '.']:
