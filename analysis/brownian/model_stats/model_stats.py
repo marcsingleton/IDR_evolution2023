0d2dd97a9e8e54fc6ed9aab99c66a83a5bd8767e
Marc Singleton <marcsingleton@berkeley.edu>
2023-04-25 10:46:28 -0700

    Fixed hiding axis labels

--- analysis/brownian/model_stats/model_stats.py
+++ analysis/brownian/model_stats/model_stats.py
@@ -253,14 +253,20 @@ for min_length in min_lengths:
                   xmin_pad=0.025, xmax_pad=0)
         ax.sharey(axs[0, 1])
         ax.set_ylabel('Disorder regions')
-        ax.set_axis_off()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        for spine in ax.spines.values():
+            spine.set_visible(False)
 
         # Heatmap
         ax = axs[0, 1]
         im = ax.imshow(array[tip_order], aspect='auto', cmap=plt.colormaps['inferno'], interpolation='none')
         ax.xaxis.set_label_position('top')
         ax.set_xlabel('Features')
-        ax.set_axis_off()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        for spine in ax.spines.values():
+            spine.set_visible(False)
 
         # Corner axis
         ax = axs[1, 0]
