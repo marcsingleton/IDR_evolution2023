f91124057014ce5648d0d0a9625332a0089d6bbc
Marc Singleton <marcsingleton@berkeley.edu>
2023-08-25 16:31:24 -0700

    Added code for OU models

--- analysis/brownian/get_sims/get_sims.py
+++ analysis/brownian/get_sims/get_sims.py
@@ -10,7 +10,7 @@ import src.phylo as phylo
 
 num_samples = 10
 models_BM = [1]
-models_OU = []
+models_OU = [(1, 0.1)]
 
 tree_template = skbio.read('../../../data/trees/consensus_LG/100R_NI.nwk', 'newick', skbio.TreeNode)
 tree_reference = tree_template.shear({tip.name for tip in tree_template.tips() if tip.name != 'sleb'})
@@ -24,9 +24,10 @@ if not os.path.exists('out/'):
 rows = []
 for sigma2 in models_BM:
     for sample_id in range(num_samples):
-        values = stats.multivariate_normal.rvs(cov=cov_reference * sigma2)
         tree = tree_reference.copy()
         tips = list(tree.tips())
+        cov = cov_reference * sigma2
+        values = stats.multivariate_normal.rvs(cov=cov)
         for tip, value in zip(tips, values):
             tip.value = value
 
@@ -41,7 +42,34 @@ for sigma2 in models_BM:
         rows.append({'sigma2': sigma2, 'sample_id': sample_id,
                      'mu_hat_BM': mu_hat_BM, 'sigma2_hat_BM': sigma2_hat_BM,
                      'loglikelihood_hat_BM': loglikelihood_hat_BM,
-                     'mu_hat_OU': mu_hat_OU, 'sigma2_hat_OU': sigma2_hat_OU,
+                     'mu_hat_OU': mu_hat_OU, 'sigma2_hat_OU': sigma2_hat_OU, 'alpha_hat_OU': alpha_hat_OU,
                      'loglikelihood_hat_OU': loglikelihood_hat_OU})
 df_BM = pd.DataFrame(rows)
 df_BM.to_csv('out/models_BM.tsv', sep='\t', index=False)
+
+# OU simulations
+rows = []
+for sigma2, alpha in models_OU:
+    for sample_id in range(num_samples):
+        tree = tree_reference.copy()
+        tips = list(tree.tips())
+        _, cov = phylo.get_OU_covariance(alpha, tips=tips, ts=cov_reference * sigma2)
+        values = stats.multivariate_normal.rvs(cov=cov)
+        for tip, value in zip(tips, values):
+            tip.value = value
+
+        mu_hat_BM, sigma2_hat_BM = phylo.get_brownian_mles(cov=cov_reference, inv=inv_reference, values=values)
+        loglikelihood_hat_BM = phylo.get_brownian_loglikelihood(mu_hat_BM, sigma2_hat_BM,
+                                                                cov=cov_reference, inv=inv_reference, values=values)
+
+        mu_hat_OU, sigma2_hat_OU, alpha_hat_OU = phylo.get_OU_mles(tips=tips, ts=cov_reference)
+        loglikelihood_hat_OU = phylo.get_OU_loglikelihood(mu_hat_OU, sigma2_hat_OU, alpha_hat_OU,
+                                                          tips=tips, ts=cov_reference)
+
+        rows.append({'sigma2': sigma2, 'alpha': alpha, 'sample_id': sample_id,
+                     'mu_hat_BM': mu_hat_BM, 'sigma2_hat_BM': sigma2_hat_BM,
+                     'loglikelihood_hat_BM': loglikelihood_hat_BM,
+                     'mu_hat_OU': mu_hat_OU, 'sigma2_hat_OU': sigma2_hat_OU, 'alpha_hat_OU': alpha_hat_OU,
+                     'loglikelihood_hat_OU': loglikelihood_hat_OU})
+df_OU = pd.DataFrame(rows)
+df_OU.to_csv('out/models_OU.tsv', sep='\t', index=False)
