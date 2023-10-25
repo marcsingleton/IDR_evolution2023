a0b8f947af8e1ffc1bed72880f35dfb12827a000
Marc Singleton <marcsingleton@berkeley.edu>
2023-10-25 09:46:05 -0700

    Various updates and fixes to plots
    More consistent use of direct log transformation of data
    Added q95 power plot
    Other small formatting changes

--- analysis/brownian/simulate_stats/simulate_stats.py
+++ analysis/brownian/simulate_stats/simulate_stats.py
@@ -7,7 +7,6 @@ import numpy as np
 import pandas as pd
 from matplotlib.cm import ScalarMappable
 from matplotlib.colors import ListedColormap, Normalize
-from numpy import linspace
 from src.brownian.simulate.sampling import sigma2_min, sigma2_max, sigma2_delta, sigma2_num
 from src.brownian.simulate.sampling import alpha_min, alpha_max, alpha_delta, alpha_num
 
@@ -27,14 +26,13 @@ if not os.path.exists('out/BM/'):
 prefix = 'out/BM/'
 
 # Violin plots of sigma2_BM
-dataset = [group['sigma2_hat_BM'].to_list() for _, group in groups_BM]
+dataset = [group['sigma2_hat_BM'].apply(np.log10).to_list() for _, group in groups_BM]
 positions = groups_BM['sigma2'].mean().apply(np.log10).to_list()
 
 fig, ax = plt.subplots()
 ax.violinplot(dataset, positions, widths=sigma2_delta/2, showmedians=True)
 ax.set_xlabel('true $\mathregular{\log_{10}(\sigma^2_{BM})}$')
-ax.set_ylabel('estimated $\mathregular{\sigma^2_{BM}}$')
-ax.set_yscale('log')
+ax.set_ylabel('estimated $\mathregular{\log_{10}(\sigma^2_{BM})}$')
 fig.savefig(f'{prefix}/violin_sigma2_hat_BM-sigma2.png')
 plt.close()
 
@@ -50,7 +48,7 @@ ax.set_xlabel('true $\mathregular{\sigma^2_{BM}}$')
 ax.set_ylabel('estimated $\mathregular{\sigma^2_{BM}}$')
 ax.set_xscale('log')
 ax.set_yscale('log')
-fig.savefig(f'{prefix}/scatter_sigma2_BM_hat-sigma2.png')
+fig.savefig(f'{prefix}/scatter_sigma2_hat_BM-sigma2.png')
 plt.close()
 
 # Violin plots of alpha
@@ -78,14 +76,13 @@ fig.savefig(f'{prefix}/scatter_alpha_hat_OU-sigma2.png')
 plt.close()
 
 # Violin plots of sigma2_OU
-dataset = [group['sigma2_hat_OU'].to_list() for _, group in groups_BM]
+dataset = [group['sigma2_hat_OU'].apply(np.log10).to_list() for _, group in groups_BM]
 positions = groups_BM['sigma2'].mean().apply(np.log10).to_list()
 
 fig, ax = plt.subplots()
-ax.violinplot(dataset, positions, widths=sigma2_delta/2, showmedians=True)
-ax.set_yscale('log')
+ax.violinplot(dataset, positions, widths=0.5*sigma2_delta, showmedians=True)
 ax.set_xlabel('true $\mathregular{\log_{10}(\sigma^2_{BM})}$')
-ax.set_ylabel('estimated $\mathregular{\sigma^2_{OU}}$')
+ax.set_ylabel('estimated $\mathregular{\log_{10}(\sigma^2_{OU})}$')
 fig.savefig(f'{prefix}/violin_sigma2_hat_OU-sigma2.png')
 plt.close()
 
@@ -117,7 +114,7 @@ plt.close()
 
 # Type I error as function of cutoff
 delta_loglikelihood_min, delta_loglikelihood_max = df_BM['delta_loglikelihood'].min(), df_BM['delta_loglikelihood'].max()
-cutoffs = linspace(delta_loglikelihood_min, delta_loglikelihood_max, 50)
+cutoffs = np.linspace(delta_loglikelihood_min, delta_loglikelihood_max, 50)
 
 errors = []
 for cutoff in cutoffs:
@@ -137,6 +134,7 @@ fig.colorbar(ScalarMappable(norm=norm, cmap=cmap), label='true $\mathregular{\lo
 fig.savefig(f'{prefix}/line_typeI-sigma2.png')
 plt.close()
 
+# Type I error as function of cutoff (merge)
 errors = []
 for cutoff in cutoffs:
     errors.append((df_BM['delta_loglikelihood'] > cutoff).mean())
@@ -145,11 +143,11 @@ q99 = df_BM['delta_loglikelihood'].quantile(0.99)
 
 fig, ax = plt.subplots(gridspec_kw={'right': 0.745})  # To match dimensions w/ colorbar; unsure why exactly is 0.745 (0.9 - 15% for colorbar is 0.765)
 ax.plot(cutoffs, errors)
-ax.axvline(q95, color='C1', label='5% type I error')
-ax.axvline(q99, color='C2', label='1% type I error')
+ax.axvline(q95, color='C1', label='5%')
+ax.axvline(q99, color='C2', label='1%')
 ax.set_xlabel('$\mathregular{\log L_{OU} - \log L_{BM}}$ cutoff')
 ax.set_ylabel('Type I error')
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+ax.legend(title='Type I error', loc='center left', bbox_to_anchor=(1, 0.5))
 fig.savefig(f'{prefix}/line_typeI-sigma2_merge.png')
 plt.close()
 
@@ -159,7 +157,9 @@ if not os.path.exists('out/OU/'):
 prefix = 'out/OU/'
 
 df_parameter = groups_OU[['sigma2_hat_OU', 'alpha_hat_OU', 'sigma2', 'alpha']].mean().sort_index(level=['sigma2_id', 'alpha_id'])
-df_error = groups_OU['delta_loglikelihood'].aggregate(lambda x: (x < q99).mean()).sort_index(level=['sigma2_id', 'alpha_id'])
+df_error = (groups_OU['delta_loglikelihood'].aggregate(**{'q95': lambda x: (x < q95).mean(),
+                                                          'q99': lambda x: (x < q99).mean()})
+                                            .sort_index(level=['sigma2_id', 'alpha_id']))
 extent = (alpha_min-alpha_delta/2, alpha_max+alpha_delta/2,
           sigma2_min-sigma2_delta/2, sigma2_max+sigma2_delta/2)
 
@@ -195,11 +195,21 @@ plt.close()
 
 # Heatmap of type II errors
 fig, ax = plt.subplots()
-array = df_error.to_numpy().reshape((sigma2_num, alpha_num))
+array = df_error['q95'].to_numpy().reshape((sigma2_num, alpha_num))
+im = ax.imshow(array, extent=extent, origin='lower', aspect='auto')
+ax.set_xlabel('true $\mathregular{\log_{10}(\\alpha)}$')
+ax.set_ylabel('true $\mathregular{\log_{10}(\sigma^2_{OU})}$')
+ax.set_title('Type II error at 5% type I error')
+fig.colorbar(im)
+fig.savefig(f'{prefix}/heatmap_typeII_q95.png')
+plt.close()
+
+fig, ax = plt.subplots()
+array = df_error['q99'].to_numpy().reshape((sigma2_num, alpha_num))
 im = ax.imshow(array, extent=extent, origin='lower', aspect='auto')
 ax.set_xlabel('true $\mathregular{\log_{10}(\\alpha)}$')
 ax.set_ylabel('true $\mathregular{\log_{10}(\sigma^2_{OU})}$')
 ax.set_title('Type II error at 1% type I error')
 fig.colorbar(im)
-fig.savefig(f'{prefix}/heatmap_typeII.png')
+fig.savefig(f'{prefix}/heatmap_typeII_q99.png')
 plt.close()
